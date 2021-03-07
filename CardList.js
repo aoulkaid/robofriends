ef8c7b987ce69ea650ddd9bd8808d82b8a3eb040
aoulkaid <80163349+aoulkaid@users.noreply.github.com>
2021-03-07 21:09:42 +0100

    Add files via upload

---
+++ CardList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList =({ robots}) => {
+	if (true){
+		throw throw new Error('NOOOOOO!');
+	}
+	return(
+ 	   <div>
+ 	     {
+	 	     robots.map((user, i) => {
+			return( 
+				<Card 
+				key={i} 
+				id={robots[i].name} 
+				name={robots[i].name} 
+				email={robots[i].email}
+				/>
+			);
+		  })
+	 	 }
+	   </div>
+);
+}
+
+export default CardList;
